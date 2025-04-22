5eafdd466be2efac1929eafd55b13a70a52eb338
MR.J <cowhdals1111@gmail.com>
2025-04-22 10:12:36 +0900

    shadcn slider 적용

--- apps/web/src/routes/book-room/-reader/page-progress.tsx
+++ apps/web/src/routes/book-room/-reader/page-progress.tsx
@@ -1,11 +1,16 @@
 import usePageInfo from "./hooks/use-page-info";
 
+import { Slider } from "#/components/ui/slider";
+
 function ReaderPageProgress() {
   const { page, total } = usePageInfo();
 
   return (
-    <div className=" h-10 w-full text-center text-black">
-      {page && total ? `${page}/${total}` : "페이지를 계산할 수 없습니다"}
+    <div className="w-full space-y-2 px-3">
+      <div className="size-full text-center text-black">
+        {page && total ? `${page}/${total}` : "페이지를 계산할 수 없습니다"}
+      </div>
+      {page && total && <Slider className="w-full" />}
     </div>
   );
 }
