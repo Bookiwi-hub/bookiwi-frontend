cce3bd9571007bd3b7c6eb1ac39affe59bf01f84
MR.J <cowhdals1111@gmail.com>
2025-04-22 16:53:46 +0900

    page button icon only hover

--- apps/web/src/routes/book-room/-component/viewer/book/index.tsx
+++ apps/web/src/routes/book-room/-component/viewer/book/index.tsx
@@ -9,13 +9,13 @@ import {
 
 function Book() {
   return (
-    <section className="relative size-full px-16 pb-16">
-      <ReaderPrevPageButton className="absolute left-0 top-0 flex h-full w-16 items-center justify-center">
-        <ChevronLeftIcon className="size-6" />
+    <section className="relative size-full px-20 pb-16">
+      <ReaderPrevPageButton className="group/button absolute left-0 top-0 flex h-full w-20 items-center justify-center">
+        <ChevronLeftIcon className="size-6 opacity-0 transition-opacity group-hover/button:opacity-100" />
       </ReaderPrevPageButton>
       <ReaderContents className="size-full" />
-      <ReaderNextPageButton className="absolute right-0 top-0 flex h-full w-16 items-center justify-center">
-        <ChevronRightIcon className="size-6" />
+      <ReaderNextPageButton className="group/button absolute right-0 top-0 flex h-full w-20 items-center justify-center">
+        <ChevronRightIcon className="size-6 opacity-0 transition-opacity group-hover/button:opacity-100" />
       </ReaderNextPageButton>
       <div className="absolute inset-x-0 bottom-0 h-16">
         <ReaderPageProgress />
