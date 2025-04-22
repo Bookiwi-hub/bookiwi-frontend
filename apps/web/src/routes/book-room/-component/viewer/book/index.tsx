1aefb4681e96468bfe61c8b65bec3353328452a8
MR.J <86304360+JayChae@users.noreply.github.com>
2025-04-22 10:27:21 +0900

    Book Slider UI (#25)

--- apps/web/src/routes/book-room/-component/viewer/book/index.tsx
+++ apps/web/src/routes/book-room/-component/viewer/book/index.tsx
@@ -9,15 +9,17 @@ import {
 
 function Book() {
   return (
-    <section className="relative flex size-full flex-col px-12">
-      <ReaderPrevPageButton className="absolute left-0 top-0 flex h-full w-12 items-center justify-center">
+    <section className="relative size-full px-16 pb-16">
+      <ReaderPrevPageButton className="absolute left-0 top-0 flex h-full w-16 items-center justify-center">
         <ChevronLeftIcon className="size-6" />
       </ReaderPrevPageButton>
       <ReaderContents className="size-full" />
-      <ReaderNextPageButton className="absolute right-0 top-0 flex h-full w-12 items-center justify-center">
+      <ReaderNextPageButton className="absolute right-0 top-0 flex h-full w-16 items-center justify-center">
         <ChevronRightIcon className="size-6" />
       </ReaderNextPageButton>
-      <ReaderPageProgress />
+      <div className="absolute inset-x-0 bottom-0 h-16">
+        <ReaderPageProgress />
+      </div>
     </section>
   );
 }
