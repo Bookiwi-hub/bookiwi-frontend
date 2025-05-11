ac4ae79ecc44a15dbb04c372a6757d08f47f070b
MR.J <cowhdals1111@gmail.com>
2025-05-11 23:24:01 +0900

    mobile correction

--- apps/web/src/routes/book-room/-mobile/index.tsx
+++ apps/web/src/routes/book-room/-mobile/index.tsx
@@ -1,4 +1,7 @@
+import { useLoaderData } from "@tanstack/react-router";
+
 import { ReaderProvider } from "../-reader";
+import { SettingsProvider } from "../-reader/settings-context";
 
 import MobileHeader from "./header";
 import MobileMenu from "./menu";
@@ -15,9 +18,14 @@ function MobileBookRoomContent() {
 }
 
 function MobileBookRoom() {
+  const { epubFile, initialSettings } = useLoaderData({
+    from: "/book-room/$id",
+  });
   return (
-    <ReaderProvider>
-      <MobileBookRoomContent />
+    <ReaderProvider epubFile={epubFile}>
+      <SettingsProvider initialSettings={initialSettings}>
+        <MobileBookRoomContent />
+      </SettingsProvider>
     </ReaderProvider>
   );
 }
