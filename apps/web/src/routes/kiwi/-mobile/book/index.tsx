332c544c463e74cb2accc3bf6a1e1e812f952371
jinjo Lim <dlawlswh123@gmail.com>
2025-05-14 16:22:50 +0900

    MobileKiwiContent에서 MobileMenu 컴포넌트를 제거하고, MobileBook에서 ReaderPageProgress의 위치를 조정하여 하단에 고정된 배경을 추가함

--- apps/web/src/routes/kiwi/-mobile/book/index.tsx
+++ apps/web/src/routes/kiwi/-mobile/book/index.tsx
@@ -2,9 +2,11 @@ import { ReaderContents, ReaderPageProgress } from "../../-reader";
 
 function MobileBook() {
   return (
-    <section className="relative flex size-full flex-col px-12">
+    <section className="relative flex size-full flex-col px-9">
       <ReaderContents className="size-full" />
-      <ReaderPageProgress />
+      <div className="absolute inset-x-0 bottom-0 h-24 bg-white">
+        <ReaderPageProgress />
+      </div>
     </section>
   );
 }
