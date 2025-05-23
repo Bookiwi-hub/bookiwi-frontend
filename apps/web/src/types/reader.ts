2db3f9b4aca484cd132c910b52d2f3f44e26f6fd
MR.J <cowhdals1111@gmail.com>
2025-05-23 17:15:16 +0900

    feat: add Metadata and StoreData interfaces to enhance file handling in reader module

--- apps/web/src/types/reader.ts
+++ apps/web/src/types/reader.ts
@@ -16,3 +16,19 @@ export interface Record {
   percentage: number | null;
   bookmarks: BookmarkItem[];
 }
+
+export interface Metadata {
+  title: string;
+  author: string;
+  publisher: string;
+  locations: string;
+}
+
+export interface StoreData {
+  id: string;
+  file: File;
+  coverImage: string | null;
+  metadata: Metadata;
+  record: Record;
+  settings: Settings;
+}
