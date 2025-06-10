6ecd4516b3841eacf73e80bf675fbd2a1d944c99
MR.J <cowhdals1111@gmail.com>
2025-06-10 20:08:26 +0900

    refactor: remove unused resizeRef in useObserver hook

--- apps/web/src/routes/kiwi/-reader/hooks/use-observer.ts
+++ apps/web/src/routes/kiwi/-reader/hooks/use-observer.ts
@@ -9,13 +9,10 @@ import { debounce } from "#/utils/debounce";
 const useObserver = () => {
   const book = useAtomValue(bookAtom);
   const prevSize = useRef(0);
-  const resizeRef = useRef<(() => void) | null>(null);
 
   const handleResize = debounce(() => {
     book?.rendition.resize();
   }, 200);
-
-  resizeRef.current = handleResize;
 
   const observer = new ResizeObserver(([e]) => {
     const size = e?.contentRect.width ?? 0;
