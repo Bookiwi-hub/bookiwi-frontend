a96cfddd2306a326b196cc269647d529671cf137
MR.J <cowhdals1111@gmail.com>
2025-06-10 16:01:33 +0900

    feat: implement text selection handling and integrate with currentView in rendered hook

--- apps/web/src/routes/kiwi/-reader/hooks/use-rendered.ts
+++ apps/web/src/routes/kiwi/-reader/hooks/use-rendered.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import IframeView from "@bookiwi/epubjs/types/managers/iframe";
 import Section from "@bookiwi/epubjs/types/section";
@@ -7,10 +7,12 @@ import { useAtomValue, useSetAtom } from "@bookiwi/jotai";
 import {
   bookAtom,
   currentSectionAtom,
+  currentViewAtom,
+  selectionAtom,
   toggleCenterTouchedAtom,
   typographyAtom,
 } from "../atoms";
-import { updateCustomStyle } from "../utils/styles";
+import { hasSelection, updateCustomStyle } from "../utils";
 
 const useToggleProgressBar = () => {
   const toggleProgressBar = useSetAtom(toggleCenterTouchedAtom);
@@ -75,9 +77,28 @@ const useUpdateCustomStyle = () => {
   }, [typography, book]);
 };
 
+const useTextSelect = () => {
+  const setSelection = useSetAtom(selectionAtom);
+  const [, render] = useState({});
+
+  const addTextSelectEvent = (iframe: Document) => {
+    iframe.addEventListener("mouseup", () => {
+      const selection = iframe.getSelection();
+      if (hasSelection(selection)) {
+        render({});
+        setSelection(selection);
+      }
+    });
+  };
+
+  return addTextSelectEvent;
+};
+
 const useRendered = () => {
   const setCurrentSection = useSetAtom(currentSectionAtom);
+  const setCurrentView = useSetAtom(currentViewAtom);
   const addProgressBarToggleEvent = useToggleProgressBar();
+  const addTextSelectEvent = useTextSelect();
 
   useUpdateCustomStyle();
 
@@ -85,9 +106,11 @@ const useRendered = () => {
     const { contents } = iframeView;
     const iframe = contents.document;
 
+    setCurrentView(iframeView);
     setCurrentSection(section);
 
     addProgressBarToggleEvent(iframe);
+    addTextSelectEvent(iframe);
   };
 
   return handleRendered;
