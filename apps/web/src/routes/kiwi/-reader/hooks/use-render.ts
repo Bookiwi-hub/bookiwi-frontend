795029d8b0b3a1d523327c99c1cd052add81336b
MR.J <cowhdals1111@gmail.com>
2025-06-12 16:18:02 +0900

    feat: enhance rendering styles with participant color and default theme adjustments

--- apps/web/src/routes/kiwi/-reader/hooks/use-render.ts
+++ apps/web/src/routes/kiwi/-reader/hooks/use-render.ts
@@ -1,11 +1,16 @@
 import { Book } from "@bookiwi/epubjs";
 import { useAtomValue } from "@bookiwi/jotai";
 
-import { initialCfiAtom, initialIsSinglePageAtom } from "../atoms";
+import {
+  initialCfiAtom,
+  initialIsSinglePageAtom,
+  participantColorAtom,
+} from "../atoms";
 
 const useRender = () => {
   const initialIsSinglePage = useAtomValue(initialIsSinglePageAtom);
   const initialCfi = useAtomValue(initialCfiAtom);
+  const participantColor = useAtomValue(participantColorAtom);
 
   const render = (node: HTMLDivElement, book: Book) => {
     // rendition 객체 생성
@@ -14,6 +19,22 @@ const useRender = () => {
       height: "100%",
       allowScriptedContent: true, // 자바스크립트 실행 허용
       spread: initialIsSinglePage ? "none" : "auto",
+    });
+
+    rendition.themes.default({
+      html: {
+        padding: "0 !important", // HTML 요소의 패딩을 0으로 강제 설정
+      },
+      body: {
+        background: "transparent", // body 배경을 투명하게 설정
+      },
+      "a:any-link": {
+        color: "#3b82f6 !important", // 모든 링크의 색상을 파란색(#3b82f6)으로 설정
+        "text-decoration": "none !important", // 링크의 밑줄을 제거
+      },
+      "::selection": {
+        "background-color": `rgba(${(participantColor ?? "#rgba(186, 230, 55)").replace(/[^\d,]/g, "")}, 0.3) !important`,
+      },
     });
     // 책 렌더링
     rendition.display(initialCfi || undefined);
