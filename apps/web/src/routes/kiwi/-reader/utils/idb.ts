61b95058708245c392b28a796baa2085320a032c
MR.J <cowhdals1111@gmail.com>
2025-06-16 18:48:15 +0900

    feat: add function to remove IDB annotations

--- apps/web/src/routes/kiwi/-reader/utils/idb.ts
+++ apps/web/src/routes/kiwi/-reader/utils/idb.ts
@@ -9,3 +9,7 @@ export const updateIDBParticipant = async (participant: ParticipantIDBData) => {
 export const addIDBAnnotation = async (annotation: AnnotationIDBData) => {
   await idb.add(IDBStore.AnnotationStore, annotation);
 };
+
+export const removeIDBAnnotation = async (id: string) => {
+  await idb.remove(IDBStore.AnnotationStore, id);
+};
