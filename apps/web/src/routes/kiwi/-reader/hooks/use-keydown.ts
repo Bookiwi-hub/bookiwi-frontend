b17ed9b9ca81e8b864482467f6d7f05da98ea11e
MR.J <cowhdals1111@gmail.com>
2025-06-18 10:31:31 +0900

    feat: add keyboard shortcut to toggle annotation pane

--- apps/web/src/routes/kiwi/-reader/hooks/use-keydown.ts
+++ apps/web/src/routes/kiwi/-reader/hooks/use-keydown.ts
@@ -1,15 +1,21 @@
-import { useAtomValue } from "@bookiwi/jotai";
+import { useAtomValue, useSetAtom } from "@bookiwi/jotai";
 
+import { toggleAnnotationPaneAtom } from "../../-split-view/atoms";
 import { bookAtom } from "../atoms";
 
 const useKeydown = () => {
   const book = useAtomValue(bookAtom);
+
+  const toggleAnnotationPane = useSetAtom(toggleAnnotationPaneAtom);
+
   // 키보드 이벤트 처리
   const handleKeyDown = (e: KeyboardEvent) => {
     if (e.code === "ArrowRight" || e.code === "ArrowDown") {
       book?.rendition.next();
     } else if (e.code === "ArrowLeft" || e.code === "ArrowUp") {
       book?.rendition.prev();
+    } else if (e.key === "m" && e.ctrlKey) {
+      toggleAnnotationPane();
     }
   };
 
