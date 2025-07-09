593696d8bd41160b2ab59c495cca55ae11184b50
MR.J <cowhdals1111@gmail.com>
2025-07-09 20:20:28 +0900

    refactor: comment out unused imports and components in MobileKiwi for cleaner code

--- apps/web/src/routes/kiwi/-mobile/index.tsx
+++ apps/web/src/routes/kiwi/-mobile/index.tsx
@@ -1,55 +1,59 @@
-import { useLoaderData } from "@tanstack/react-router";
+// import { useLoaderData } from "@tanstack/react-router";
 
-import AddParticipantModal from "../-modals/add-participant";
-import { ReaderProvider } from "../-reader";
+// import AddParticipantModal from "../-modals/add-participant";
+// import { ReaderProvider } from "../-reader";
 
-import MobileHeader from "./header";
-import MobileMenu from "./menu";
-import MobileViewer from "./viewer";
+// import MobileHeader from "./header";
+// import MobileMenu from "./menu";
+// import MobileViewer from "./viewer";
 
-import userManager from "#/managers/user";
+// import userManager from "#/managers/user";
 
-function MobileKiwiContent() {
-  return (
-    <div className="size-full">
-      <MobileHeader />
-      <MobileViewer />
-      <MobileMenu />
-    </div>
-  );
-}
+// function MobileKiwiContent() {
+//   return (
+//     <div className="size-full">
+//       <MobileHeader />
+//       <MobileViewer />
+//       <MobileMenu />
+//     </div>
+//   );
+// }
+
+// function MobileKiwi() {
+//   const { epubData, kiwiData, participantsData, annotationsData } =
+//     useLoaderData({
+//       from: "/kiwi/$id",
+//     });
+
+//   const currentParticipant = participantsData.find(
+//     (participant) => participant.userId === userManager.userId,
+//   );
+
+//   if (!currentParticipant) {
+//     return (
+//       <AddParticipantModal
+//         kiwiName={kiwiData.name}
+//         kiwiId={kiwiData.id}
+//         takenColors={participantsData.map((participant) => participant.color)}
+//       />
+//     );
+//   }
+
+//   return (
+//     <ReaderProvider
+//       epubData={epubData}
+//       kiwiData={kiwiData}
+//       currentParticipant={currentParticipant}
+//       participantsData={participantsData}
+//       annotationsData={annotationsData}
+//     >
+//       <MobileKiwiContent />
+//     </ReaderProvider>
+//   );
+// }
 
 function MobileKiwi() {
-  const { epubData, kiwiData, participantsData, annotationsData } =
-    useLoaderData({
-      from: "/kiwi/$id",
-    });
-
-  const currentParticipant = participantsData.find(
-    (participant) => participant.userId === userManager.userId,
-  );
-
-  if (!currentParticipant) {
-    return (
-      <AddParticipantModal
-        kiwiName={kiwiData.name}
-        kiwiId={kiwiData.id}
-        takenColors={participantsData.map((participant) => participant.color)}
-      />
-    );
-  }
-
-  return (
-    <ReaderProvider
-      epubData={epubData}
-      kiwiData={kiwiData}
-      currentParticipant={currentParticipant}
-      participantsData={participantsData}
-      annotationsData={annotationsData}
-    >
-      <MobileKiwiContent />
-    </ReaderProvider>
-  );
+  return <div>MobileKiwi</div>;
 }
 
 export default MobileKiwi;
