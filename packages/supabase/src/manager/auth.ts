e587a7ad2daa2894e7720887c063f82399cc05f9
MR.J <cowhdals1111@gmail.com>
2025-08-09 12:09:26 +0900

    feat: add signInWithGoogle method to SupabaseAuth class

--- packages/supabase/src/manager/auth.ts
+++ packages/supabase/src/manager/auth.ts
@@ -25,16 +25,26 @@ class SupabaseAuth {
     return data;
   }
 
+  async signInWithGoogle(
+    options?: Omit<SignInWithOAuthCredentials, "provider">,
+  ) {
+    const { data, error } = await this.supabase.auth.signInWithOAuth({
+      provider: "google",
+      ...options,
+    });
+    if (error) {
+      throw new Error(error.message);
+    }
+    return data;
+  }
+
   async getUser(): Promise<User | null> {
     const { data } = await this.supabase.auth.getUser();
+    console.log(data);
 
     if (!data.user) return null;
     const { id } = data.user;
-    const {
-      email,
-      user_name: name,
-      avatar_url: profileImage,
-    } = data.user.user_metadata;
+    const { email, name, avatar_url: profileImage } = data.user.user_metadata;
 
     return {
       id,
